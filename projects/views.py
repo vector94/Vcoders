9cab5c485075eaca37b012379c595382f4b31faa
Asif Ahmed <asif.ahmed2@northsouth.edu>
2021-09-28 02:44:02 +0600

    User Login, Logout

--- projects/views.py
+++ projects/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
-
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 from .models import Project
 from .forms import ProjectForm
 
@@ -16,6 +16,7 @@ def project(request, pk):
     return render(request, 'projects/single-project.html', {'project': projectObj})
 
 
+@login_required(login_url="login")
 def createProject(request):
     form = ProjectForm()
 
@@ -29,6 +30,7 @@ def createProject(request):
     return render(request, "projects/project_form.html", context)
 
 
+@login_required(login_url="login")
 def updateProject(request, pk):
     project = Project.objects.get(id=pk)
     form = ProjectForm(instance=project)
@@ -43,6 +45,7 @@ def updateProject(request, pk):
     return render(request, "projects/project_form.html", context)
 
 
+@login_required(login_url="login")
 def deleteProject(request, pk):
     project = Project.objects.get(id=pk)
     if request.method == 'POST':
