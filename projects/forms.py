7451b4332702d297ab94019f5b1238a1eae01102
Asif Ahmed <asif.ahmed2@northsouth.edu>
2021-09-27 07:57:55 +0600

    Update Form

--- projects/forms.py
+++ projects/forms.py
@@ -1,4 +1,6 @@
+import form as form
 from django.forms import ModelForm
+from django import forms
 from .models import Project
 
 
@@ -6,3 +8,15 @@ class ProjectForm(ModelForm):
     class Meta:
         model = Project
         fields = ['title', 'featured_image', 'description', 'demo_link', 'source_link', 'tags']
+        widgets = {
+            'tags': forms.CheckboxSelectMultiple(),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(ProjectForm, self).__init__(*args, **kwargs)
+
+        for name, field in self.fields.items():
+            field.widget.attrs.update({'class': 'input'})
+
+        # self.fields['title'].widget.attrs.update({'class': 'input'})
+
