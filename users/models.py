50869b8e05c0fd8a3190c50bb6333959444ea77f
Asif Ahmed <asif.ahmed2@northsouth.edu>
2021-09-27 21:53:02 +0600

    Create Signals

--- users/models.py
+++ users/models.py
@@ -3,6 +3,9 @@ from django.contrib.auth.models import User
 import uuid
 # Create your models here.
 
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
@@ -22,7 +25,7 @@ class Profile(models.Model):
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
 
     def __str__(self):
-        return str(self.user.username)
+        return str(self.username)
 
 
 class Skill(models.Model):
@@ -34,3 +37,24 @@ class Skill(models.Model):
 
     def __str__(self):
         return str(self.name)
+
+
+def createProfile(sender, instance, created, **kwargs):
+    if created:
+        user = instance
+        profile = Profile.objects.create(
+            user=user,
+            username=user.username,
+            email=user.email,
+            name=user.first_name,
+        )
+
+
+def deleteUser(sender, instance, **kwargs):
+    user = instance.user
+    user.delete()
+
+
+post_save.connect(createProfile, sender=User)
+
+post_delete.connect(deleteUser, sender=Profile)
